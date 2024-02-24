ba4075dbbf262a574fe86a5e90aedca1b979dd98
Anthony Chavez <anthony@anthonychavez.xyz>
2024-02-24 17:06:24 -0600

    Read in a file's contents

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, fs};
+
+fn read_file(path: &str) -> Vec<u8> {
+    let contents: Result<Vec<u8>, std::io::Error> = fs::read(path);
+    match contents {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("Error opening file: {}", e.to_string());
+            std::process::exit(2);
+        },
+    }
+}
+
+fn main() -> Result<(), String> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        return Err("missing file operand".to_string());
+    }
+    let file_contents: Vec<u8> = read_file(&args[1]);
+    Ok(())
 }
